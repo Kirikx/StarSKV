a764029f5d662c4f829fbae22d1a0b6c0ce20c93
kirikx <kirikx@mail.ru>
2020-02-19 21:05:00 +0300

    Картинка следует за кликом курсора пока не встретится с верхней границей экрана (оставил пока что данное условие)

--- core/src/ru/skv/screen/MenuScreen.java
+++ core/src/ru/skv/screen/MenuScreen.java
@@ -13,6 +13,7 @@ public class MenuScreen extends BaseScreen {
     private Vector2 touch;
     private Vector2 v;
     private Vector2 pos;
+    private Vector2 cursor;
 
     @Override
     public void show() {
@@ -21,6 +22,7 @@ public class MenuScreen extends BaseScreen {
         touch = new Vector2();
         v = new Vector2(1, 1);
         pos = new Vector2(0, 0);
+        cursor = new Vector2(0,0);
     }
 
     @Override
@@ -29,7 +31,7 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClearColor(1, 0.2f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         if (pos.y + img.getHeight() < Gdx.graphics.getHeight()) {
-            pos.add(v);
+            pos.add(cursor);
         }
         batch.begin();
         batch.draw(img, pos.x, pos.y);
@@ -46,6 +48,8 @@ public class MenuScreen extends BaseScreen {
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         super.touchDown(screenX, screenY, pointer, button);
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        cursor.set(touch);
+        cursor.set(cursor.sub(pos).nor()); // заначение курсора получаем путем вычитания из текущего значения курсора позиции элемента и нормализуем
         System.out.println("touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
